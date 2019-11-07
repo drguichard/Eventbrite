46027ecdc11cd8a9742c6008adf49d29196d7cf8
TG <tguichard8@hotmail.com>
2019-11-07 18:15:35 +0100

    First commit and pushing to Heroku2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
 require 'faker'
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 p "---------- START CLEAN THE DATABASE ----------"
 User.destroy_all
 p "Users are clean"
@@ -14,10 +7,7 @@ p "Events are clean"
 #Attendance.destroy_all
 #p "Attendances are clean"
 p "---------- END CLEAN THE DATABASE ----------"
-p ""
-p ""
 p "---------------- START SEED ----------------"
-p ""
 User.create(first_name: "Loulou", last_name:Faker::Name.last_name, description:Faker::Lorem.paragraph, email: "loulou@yopmail.com",password:"password", password_confirmation:"password")
 puts 'User created'
 20.times do
