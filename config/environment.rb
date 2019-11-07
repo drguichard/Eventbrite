87717c5ea5ec910d3e2ee9980de1a0324d81f299
TG <tguichard8@hotmail.com>
2019-11-07 22:26:57 +0100

    from sendgrid to gmail5

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,11 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "gmail.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
-}
+ActionMailer::Base.smtp_settings =   {
+  :address            => 'smtp.gmail.com',
+  :port               => 587,
+  :domain             => 'gmail.com', #you can also use google.com
+  :authentication     => :plain,
+  :user_name          => ENV['GMAIL_LOGIN'],
+  :password           => ENV['GMAIL_PWD']
 }
