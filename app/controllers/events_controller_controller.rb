34ae1cd926015110de149cdcf7c5548ab4e85676
TG <tguichard8@hotmail.com>
2019-11-07 17:08:26 +0100

    navbar creation

--- app/controllers/events_controller_controller.rb
+++
@@ -1,4 +0,0 @@
-class EventsControllerController < ApplicationController
-  def index
-  end
-end
--- test/controllers/events_controller_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EventsControllerControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get events_controller_index_url
-    assert_response :success
-  end
-
-end
