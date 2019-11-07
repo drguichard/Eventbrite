94525fccce25f7752606c372f7321142d9eea758
TG <tguichard8@hotmail.com>
2019-11-07 17:51:28 +0100

    homepage view done, need more links path, bd ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -17,27 +18,9 @@ p ""
 p ""
 p "---------------- START SEED ----------------"
 p ""
-​
-User.create(
-	first_name: Faker::Name.first_name,
-	last_name:Faker::Name.last_name,
-	description:Faker::Lorem.paragraph,
-	email: "jean@yopmail.com",
-	password:"password"
-)
-​
+User.create(first_name: "Loulou", last_name:Faker::Name.last_name, description:Faker::Lorem.paragraph, email: "loulou@yopmail.com",password:"password", password_confirmation:"password")
 puts 'User created'
-​
 20.times do
-	Event.create(
-		start_date: Faker::Date.forward(days: 365),
-		duration: rand(6..100)*5,
-		admin_id: User.all.sample.id,
-		price: rand(5..1000),
-		description:Faker::Lorem.paragraph,
-		title: Faker::Movies::StarWars.quote,
-		location: Faker::Address.city
-		)
+	Event.create(start_date: Faker::Date.forward(days: 365), duration: rand(6..1000)*5, administrator_id: User.all.sample.id, price: rand(5..1000), description:Faker::Lorem.paragraph, title: Faker::Movies::StarWars.quote, location: Faker::Address.city)
 end
-​
-puts 'The database has now #{Event.count} events'
+puts 'Events created'
