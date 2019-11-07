33f0513c7f8932f81464ca80b5400c6b05b5def2
TG <tguichard8@hotmail.com>
2019-11-07 14:06:19 +0100

    send email after user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
 #Un utilisateur peut participer à plusieurs événements au travers des participations. Un utilisateur peut administrer plusieurs événements.
-    has_many :attendances
+  has_many :attendances
 	has_many :events
 	has_many :events, through: :attendances
-	
-	validates :first_name, presence: true
-	validates :last_name, presence: true
+	  
+    after_create :welcome_send #après création d'un user faire appele à la méthide welcome send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now #appeler le mailer UserMailer en lui faisant exécuter welcome_email avec, pour seule entrée, l'instance créée (d'où le self); deliver_now pour envoyer immédiatement l’e-mail. Il est possible d'utiliser un deliver_later
+  end
+
+
 end
