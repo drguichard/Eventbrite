dc82faeca4cf73accb4d2fc804655e869680214b
TG <tguichard8@hotmail.com>
2019-11-08 12:10:12 +0100

    seed add 3 administrator events and some entries in page profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
     before_action :authenticate_user! #ondoit être loggué ou inscrit pour accéder au site
 
+    def show 
+        @user=User.find(params[:id])
+        @events=Event.where(administrator_id:@user.id)
+    end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,28 @@ p ""
 p "---------------- START SEED ----------------"
 p ""
 User.create(
-	first_name: Faker::Name.first_name,
-	last_name:Faker::Name.last_name,
-	description:Faker::Lorem.paragraph,
+	first_name: "Thibault",
+	last_name: "Guichard",
+	description: "Semi-professionnel en AquaPoneyBall j'organise des tournois de bilboquet",
 	email: "tguichard8@hotmail.com",
 	password:"password"
 )
 puts 'User created'
+
+3.times do
+Event.create(
+	start_date: Faker::Date.forward(days: 365),
+	duration: rand(6..100)*5,
+	administrator_id: User.first_name("Thibault"),
+	price: rand(5..1000),
+	description:Faker::Lorem.paragraph,
+	title: Faker::Movies::StarWars.quote,
+	location: Faker::Address.city
+	)
+end
+puts 'Events de adminitrator created'
+
+
 30.times do
 	Event.create(
 		start_date: Faker::Date.forward(days: 365),
